e89134256387128ae595b3466ab60def47c2eba9
engsr6982 <pwxg286704@163.com>
2024-04-27 16:33:10 +0800

    Update DocCardList.js

--- src/theme/DocCardList/index.js
+++ src/theme/DocCardList/index.js
@@ -1,22 +1,26 @@
-import React from 'react';
-import clsx from 'clsx';
+import React from "react";
+import clsx from "clsx";
 import {
   useCurrentSidebarCategory,
   filterDocCardListItems,
-} from '@docusaurus/theme-common';
-import DocCard from '@theme/DocCard';
-function DocCardListForCurrentSidebarCategory({className}) {
+} from "@docusaurus/theme-common";
+import DocCard from "@theme/DocCard";
+
+function DocCardListForCurrentSidebarCategory({ className }) {
   const category = useCurrentSidebarCategory();
   return <DocCardList items={category.items} className={className} />;
 }
-export default function DocCardList(props) {
-  const {items, className} = props;
+
+function DefaultDocCardList(props) {
+  console.warn("DefaultDocCardList: ", JSON.stringify(props, null, 2));
+
+  const { items, className } = props;
   if (!items) {
     return <DocCardListForCurrentSidebarCategory {...props} />;
   }
   const filteredItems = filterDocCardListItems(items);
   return (
-    <section className={clsx('row', className)}>
+    <section className={clsx("row", className)}>
       {filteredItems.map((item, index) => (
         <article key={index} className="col col--6 margin-bottom--lg">
           <DocCard item={item} />
@@ -24,4 +28,39 @@ export default function DocCardList(props) {
       ))}
     </section>
   );
+}
+
+function MyDocCardList({ props }) {
+  console.warn("MyDocCardList: ", JSON.stringify(props, null, 2));
+
+  props.className =
+    "list_node_modules-@docusaurus-theme-classic-lib-theme-DocCategoryGeneratedIndexPage-styles-module";
+
+  const { items, className } = props;
+
+  return (
+    <section className={clsx("row", className)}>
+      {items.map((item, index) => (
+        <article key={index} className="col col--6 margin-bottom--lg">
+          <DocCard item={item} />
+        </article>
+      ))}
+    </section>
+  );
+}
+
+const hasKey = (obj, key) => {
+  return Object.prototype.hasOwnProperty.call(obj, key);
+};
+
+export default function DocCardList(props) {
+  console.warn("DocCardList: ", JSON.stringify(props, null, 2));
+
+  if (hasKey(props, "props")) {
+    if (props.props.custom) {
+      return <MyDocCardList {...props} />;
+    }
+  } else {
+    return <DefaultDocCardList {...props} />;
+  }
 }
