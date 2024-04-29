909e55c21199d27a8ccf1bfb9fdf3c14b6cb4dfd
engsr6982 <109733049+engsr6982@users.noreply.github.com>
2024-04-29 23:36:24 +0800

    v4

--- pages/Serach/index.jsx
+++ pages/Serach/index.jsx
@@ -1,17 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import Layout from '@theme/Layout';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import Layout from "@theme/Layout";
+import axios from "axios";
 
 export default function SearchPage() {
   const [data, setData] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
-      const result = await axios('https://raw.githubusercontent.com/engsr6982/Minecraft-Bedrock_ID-SQLite/main/JSON/mcid_old.json');
+      const result = await axios(
+        "https://raw.githubusercontent.com/engsr6982/Minecraft-Bedrock_ID-SQLite/main/JSON/mcid_old.json"
+      );
       setData(result.data.mcid);
       setIsLoading(false);
     };
@@ -20,41 +22,84 @@ export default function SearchPage() {
   }, []);
 
   useEffect(() => {
-    if (searchTerm !== '') {
-      const results = data.filter(item =>
+    if (searchTerm !== "") {
+      const results = data.filter((item) =>
         item.name.toLowerCase().includes(searchTerm.toLowerCase())
       );
       setFilteredData(results);
     } else {
-      setFilteredData(data.slice(0, 5)); // 默认显示5个
+      setFilteredData(data.slice(0, 9)); // 默认显示9个
     }
   }, [searchTerm, data]);
 
   return (
     <Layout title="SearchPage" description="SearchPage React Page">
-      <div style={{ padding: '20px', height: 'auto' }}> 
-        <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
+      <div style={{ padding: "20px", height: "auto" }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginBottom: "20px",
+          }}
+        >
           <input
             type="text"
             placeholder="搜索物品..."
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
-            style={{ width: '100%', padding: '10px' }}
+            style={{ width: "100%", padding: "10px" }}
           />
         </div>
-        <div style={{ maxHeight: '60vh', overflowY: 'auto', display: 'flex', flexWrap: 'wrap', justifyContent: 'flex-start' }}>
+        <div
+          style={{
+            maxHeight: "60vh",
+            overflowY: "auto",
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "flex-start",
+          }}
+        >
           {isLoading ? (
             <p>Loading...</p>
           ) : (
-            filteredData.map(item => (
-              <div key={item.id} style={{ border: '1px solid #ccc', padding: '10px', marginBottom: '10px', borderRadius: '5px', display: 'flex', flexDirection: 'column', width: 'calc(20% - 20px)', minWidth: '400px', maxWidth: 'calc(100% / 5 - 20px)', minHeight: '180px', maxHeight: '220px', margin: '0 10px 10px 0' }}>
-                <img src={item.img || 'path/to/default/image.png'} alt="" style={{ width: '50px', marginRight: '10px', objectFit: 'cover', alignSelf: 'flex-end' }} onError={(e) => { e.target.onerror = null; e.target.src = 'path/to/default/image.png'; }} />
-                <div style={{ marginTop: '5px', padding: '5px' }}>
-                  <h4 style={{ margin: '5px 0' }}>{item.name}</h4>
-                  <p style={{ margin: '2px 0' }}>ID: {item.id}</p>
-                  <p style={{ margin: '2px 0' }}>DV: {item.dv}</p>
-                  <p style={{ margin: '2px 0' }}>Type: {item.type}</p>
-                  <p style={{ margin: '2px 0' }}>Path: {item.path}</p>
+            filteredData.map((item) => (
+              <div
+                key={item.id}
+                style={{
+                  border: "1px solid #ccc",
+                  padding: "10px",
+                  marginBottom: "10px",
+                  borderRadius: "5px",
+                  display: "flex",
+                  flexDirection: "column",
+                  flex: "1 1 auto",
+                  minWidth: "360px",
+                  maxWidth: "420px",
+                  minHeight: "180px",
+                  maxHeight: "220px",
+                  margin: "0 10px 10px 0",
+                }}
+              >
+                <img
+                  src={item.img || "path/to/default/image.png"}
+                  alt=""
+                  style={{
+                    width: "50px",
+                    marginRight: "10px",
+                    objectFit: "cover",
+                    alignSelf: "flex-end",
+                  }}
+                  onError={(e) => {
+                    e.target.onerror = null;
+                    e.target.src = "path/to/default/image.png";
+                  }}
+                />
+                <div style={{ marginTop: "5px", padding: "5px" }}>
+                  <h4 style={{ margin: "5px 0" }}>{item.name}</h4>
+                  <p style={{ margin: "2px 0" }}>ID: {item.id}</p>
+                  <p style={{ margin: "2px 0" }}>DV: {item.dv}</p>
+                  <p style={{ margin: "2px 0" }}>Type: {item.type}</p>
+                  <p style={{ margin: "2px 0" }}>Path: {item.path}</p>
                 </div>
               </div>
             ))
